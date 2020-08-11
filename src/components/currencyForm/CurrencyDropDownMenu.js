e51dbbf5367f2e356f9512e875b72d4df7bc8d1a
bertamatu <info.berta@gmail.com>
2020-08-11 12:49:57 +0300

    select currency

--- src/components/currencyForm/CurrencyDropDownMenu.js
+++ src/components/currencyForm/CurrencyDropDownMenu.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Select from "react-select";
 
 const CurrencyDropDownMenu = (props) => {
   const { currencyRatesArray } = props;
+  const [selectedOption, setSelectedOption] = useState();
 
-  const currencyOptions = currencyRatesArray.map((item, index) => {
+  const currencyOptions = currencyRatesArray.map((item) => {
     return {
       label: item.code,
-      value: item.code,
-      key: index,
+      value: item,
     };
   });
+
+  const handleOptionSelect = (selectedOption) => {
+    setSelectedOption({ selectedOption });
+    console.log("selectedOption", selectedOption, selectedOption.value);
+  };
 
   return (
     <div>
       <b>Add currency:</b>
       <div style={{ width: "200px" }}>
-        <Select options={currencyOptions} />
+        <Select
+          value={selectedOption}
+          selected={currencyOptions === selectedOption}
+          options={currencyOptions}
+          onChange={handleOptionSelect}
+        />
       </div>
+      <button type="submit">Add</button>
     </div>
   );
 };
